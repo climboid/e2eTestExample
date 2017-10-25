3d83046a9de6089137d1413743dfc81d4375e34d
Oscar Villarreal <oscarvillarreal14@gmail.com>
2017-10-25 12:12:53 -0500

    adding gitignore updates

--- tests/01_homePage.js
+++ tests/01_homePage.js
@@ -9,14 +9,14 @@ module.exports = {
     browser
       .url('http://www.google.com')
       .waitForElementVisible('#body')
-      .pause(5000)
+      .pause(1000)
       .assert.visible('#body')
       .assert.visible('input[type="submit"]')
       .setValue('#lst-ib', 'nightwatch')
-      .pause(5000)
+      .pause(1000)
       .assert.visible('.sbsb_b')
       .click('.sbsb_c.gsfs:first-child')
-      .pause(5000)
+      .pause(2000)
       .assert.containsText('h3.r:first-child','Nightwatch.js | Node.js powered End-to-End testing framework')
       .end();
   }
